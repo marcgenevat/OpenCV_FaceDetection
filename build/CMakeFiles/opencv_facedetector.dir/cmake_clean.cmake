f011aad627a0eacf4669863d4dcd2cd4d95bbe0c
Marc Genevat <mgenevat@gmail.com>
2015-12-03 01:34:31 +0100

    Uploading exercise

---
+++ build/CMakeFiles/opencv_facedetector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/opencv_facedetector.dir/src/opencv_facedetector.cpp.o"
+  "opencv_facedetector.pdb"
+  "opencv_facedetector"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/opencv_facedetector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
